2c1764e09e692c3f46436c8b6c2be193428b53a6
Kevin Rangel Moreira <rangel.moreira@outlook.com>
2021-04-20 14:42:34 -0300

    Header layout and podcast player layout added and SSG request

---
+++ src/pages/index.tsx
@@ -0,0 +1,36 @@
+
+import {useEffect} from "react";
+
+export default function Home(props) {
+  console.log(props.episodes);
+  return (
+    <h1>Index</h1>
+  )
+}
+
+//SSR
+/*
+export async function getServerSideProps(){
+  const response =  await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return{
+    props: {
+      episodes: data,
+    }
+  }
+  
+}*/
+
+//SSG
+export async function getStaticProps(){
+  const response =  await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return{
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  }
+}
